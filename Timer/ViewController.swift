4ed550318c171e240b722deb166abe8e167f8c5d
AGREE <agree@AGREEs-iMac.local>
2019-01-24 10:03:28 -0800

    create functions for button

--- Timer/ViewController.swift
+++ Timer/ViewController.swift
@@ -7,14 +7,67 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    var timer = Timer()
+    var audioplay = AVAudioPlayer()
 
+    @IBOutlet weak var input: UITextField!
+    
+    var time = 0
+   
+    @objc func decreaseTimer() {
+        
+        if time > 0 {
+            
+            time -= 1
+            
+            timelabel.text = String(time)
+            
+        } else {
+            audioplay.play()
+            timer.invalidate()
+            
+        }
+        
+    }
+    @IBOutlet weak var timelabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        do {
+            let path = Bundle.main.path(forResource: "sound", ofType: ".mp3")
+            try audioplay = AVAudioPlayer(contentsOf: URL(fileURLWithPath: path!))
+        }
+        catch{
+            
+        }
     }
-
-
+    @IBAction func minus(_ sender: Any) {
+        if time > 5 {
+            time -= 5
+            timelabel.text = String(time)
+        }
+    }
+    
+    @IBAction func plus(_ sender: Any) {
+        if time != 0{time += 5
+            timelabel.text = String(time)}
+    }
+    
+    @IBAction func start(_ sender: Any) {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.decreaseTimer), userInfo: nil, repeats: true)    }
+    
+    @IBAction func reset(_ sender: Any) {
+        timelabel.text = String(time)
+        timer.invalidate()
+    }
+    @IBAction func stop(_ sender: Any) {
+        timer.invalidate()
+        audioplay.stop()
+    }
+    
+    
 }
 
